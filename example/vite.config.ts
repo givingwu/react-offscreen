0576630e85e78793dd914c35da9e95d445ecad1e
mountainwu <mountainwu@kuainiugroup.com>
2024-12-18 13:28:20 +0800

    chore: added github-page env

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -1,9 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// 判断是否在 GitHub Pages 环境
+const isGitHubPages = process.env.DEPLOY_ENV === "gh-pages";
+
+console.log("isGitHubPages: ", isGitHubPages);
+
 export default defineConfig({
 	plugins: [react()],
-	base: "/",
+	base: isGitHubPages ? "/react-offscreen/" : "/",
 	build: {
 		outDir: "dist",
 		assetsDir: "assets",
